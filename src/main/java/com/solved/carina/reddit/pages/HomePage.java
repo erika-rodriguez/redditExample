bc9edf99a0949f4687e7f4827c31ba7f5146e13c
Erika Rodriguez <rodriguez.eri92@gmail.com>
2023-12-12 15:02:14 -0300

    Logger implementation on HomePage class

--- src/main/java/com/solved/carina/reddit/pages/HomePage.java
+++ src/main/java/com/solved/carina/reddit/pages/HomePage.java
@@ -6,8 +6,12 @@ import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.logging.Logger;
+
 @DeviceType(pageType = DeviceType.Type.DESKTOP,parentClass = HomePageBase.class)
 public class HomePage extends HomePageBase {
+    private static final Logger LOGGER = Logger.getLogger(String.valueOf(HomePage.class));
     @FindBy(xpath = "//a[contains(@href,'login')]")
     //"//div[@id='email-verification-tooltip-id']/..//a"
     private ExtendedWebElement loginBtn;
@@ -24,7 +28,7 @@ public class HomePage extends HomePageBase {
     }
     @Override
     public String getPostTitle() {
-        System.out.println(firstPostTitle.getText());
+        LOGGER.info(firstPostTitle.getText());
         return firstPostTitle.getText();
     }
 }
